94442698b3c8e60f16a86c84e5b1fe5f9f0a3678
Kevin Collins <kevincol54@gmail.com>
2014-05-22 23:02:14 -0400

    more speech reocgnition with showing and hiding comments, fixed copmments ajax.

--- forum/homework1/app/assets/javascripts/application.js
+++ forum/homework1/app/assets/javascripts/application.js
@@ -21,22 +21,49 @@ var when_page_is_ready = function(){
   $("p.notice").fadeOut(2000)
 
 
+  // $("div.commentsheader").click(function() {
+  //   $("div.commentslist").slideUp('slow');
+
+  // })
+  $("div.commentslist").hide();
+  if (annyang) {
+    var commands = {
+        'Show Comments': function() {
+          $("div.commentslist").slideDown('slow');
+        }
+    };
+    annyang.addCommands(commands);
+    annyang.start(); 
+  }
+
+  if (annyang) {
+    var commands = {
+        'Hide Comments': function() {
+          $("div.commentslist").slideUp('slow');
+        }
+    };
+    annyang.addCommands(commands);
+    // annyang.start(); 
+  }
+
+ 
+
 
  
 }
 
 
+// if (annyang) {
+//     var commands = {
+//         'Kevin': function() {
+//             alert('I implemented this after drinking two bottles of champagne!');
+//         }
+//     };
+//     annyang.addCommands(commands);
+//     annyang.start(); 
+// }
 
-      if (annyang) {
-          var commands = {
-              'Kevin': function() {
-                  alert('I implemented this after drinking two bottles of champagne!');
-              }
-          };
-          annyang.addCommands(commands);
-          annyang.start(); 
-      }
-    
+ 
 
 $(document).ready(when_page_is_ready);
 $(document).on("page:load", when_page_is_ready);
